515b1d230e9e41bceec0f487ddb48b3d0718ff01
pkolt <pkoltyshev@gmail.com>
2023-12-13 21:06:12 +0400

    Fix bug

--- include/bitwise.h
+++ include/bitwise.h
@@ -2,10 +2,11 @@
 #define BIT_WISE_H
 
 // Macros bit operations
+// #define _BV(bit) (1 << (bit))
 #define set_bit(reg, bit) (reg |= _BV(bit))
 #define clear_bit(reg, bit) (reg &= ~_BV(bit))
 #define invert_bit(reg, bit) (reg ^= _BV(bit))
-#define copy_bit(src_reg, dst_reg, bit) (bit_is_set(src_reg, bit) ? set_bit(dst_reg, bit) : clear_bit(dst_reg, bit))
 #define is_set_bit(reg, bit) (reg & _BV(bit))
+#define copy_bit(src_reg, dst_reg, bit) (is_set_bit(src_reg, bit) ? set_bit(dst_reg, bit) : clear_bit(dst_reg, bit))
 
 #endif // BIT_WISE_H
